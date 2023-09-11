2ab7ec2682f6f63ae52ed99961582ab4bcaf8167
swasthikka <swasthikka@gmail.com>
2023-09-11 15:12:36 +0530

    edited textbox for searching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,30 @@
-import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
 const Home = () => {
-  return <div>Home</div>;
+  const [searchStr, setSearchStr] = useState('');
+
+  const onSearchStr = ev => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onSearch = async ev => {
+    ev.preventDefault();
+
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+    const body = await response.json();
+    console.log(body);
+
+    // https://api.tvmaze.com/search/shows?q=girls
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchStr} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 export default Home;
