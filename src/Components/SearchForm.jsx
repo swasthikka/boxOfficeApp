1695dceaf07324f39028abdea94f713e247b5cbf
swasthikka <swasthikka@gmail.com>
2023-09-12 10:33:00 +0530

    isolated search form logic

---
+++ src/Components/SearchForm.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+const SearchForm = ({ onSearch }) => {
+  const [searchStr, setSearchStr] = useState('');
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const onSearchInputChange = ev => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
+  const onSubmit = ev => {
+    ev.preventDefault();
+    const options = {
+      q: searchStr,
+      searchOption,
+    };
+    onSearch(options);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" value={searchStr} onChange={onSearchInputChange} />
+
+      <label>
+        Shows
+        <input
+          type="radio"
+          name="search-option"
+          value="shows"
+          checked={searchOption === 'shows'}
+          onChange={onRadioChange}
+        ></input>
+      </label>
+
+      <label>
+        Actors
+        <input
+          type="radio"
+          name="search-option"
+          value="actors"
+          checked={searchOption === 'actors'}
+          onChange={onRadioChange}
+        ></input>
+      </label>
+
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+export default SearchForm;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,22 @@
 import { useState } from 'react';
 import { searchForShows, searchForPeople } from '../api/tvmaze';
+import SearchForm from '../Components/SearchForm';
 
 const Home = () => {
-  const [searchStr, setSearchStr] = useState('');
   const [apiData, setApiData] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
-  const [searchOption, setSearchOption] = useState('shows');
 
-  const onSearchInputChange = ev => {
-    setSearchStr(ev.target.value);
-  };
-
-  const onRadioChange = ev => {
-    setSearchOption(ev.target.value);
-  };
-
-  const onSearch = async ev => {
-    ev.preventDefault();
-
+  const onSearch = async ({ q, searchOption }) => {
     try {
+      setApiDataError(null);
+
+      let result;
+
       if (searchOption === 'shows') {
-        const result = await searchForShows(searchStr);
+        result = await searchForShows(q);
         setApiData(result);
       } else {
-        const result = await searchForPeople(searchStr);
+        result = await searchForPeople(q);
         setApiData(result);
       }
     } catch (error) {
@@ -48,33 +41,7 @@ const Home = () => {
 
   return (
     <div>
-      <form onSubmit={onSearch}>
-        <input type="text" value={searchStr} onChange={onSearchInputChange} />
-
-        <label>
-          Shows
-          <input
-            type="radio"
-            name="search-option"
-            value="shows"
-            checked={searchOption === 'shows'}
-            onChange={onRadioChange}
-          ></input>
-        </label>
-
-        <label>
-          Actors
-          <input
-            type="radio"
-            name="search-option"
-            value="actors"
-            checked={searchOption === 'actors'}
-            onChange={onRadioChange}
-          ></input>
-        </label>
-
-        <button type="submit">Search</button>
-      </form>
+      <SearchForm onSearch={onSearch} />
       <div>{renderApiData()}</div>
     </div>
   );
