8eeb65044d9c809a3094e13f18093d717b5d8c82
swasthikka <swasthikka@gmail.com>
2023-09-13 19:38:39 +0530

    added custom hook

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
 
-const Show = () => {
-  const { showId } = useParams();
-
+//custom hook
+const useShowById = showId => {
   const [showData, setShowData] = useState(null);
-  const [showError, setShowErrr] = useState(null);
+  const [showError, setShowError] = useState(null);
+
   useEffect(() => {
     async function fetchData() {
       try {
@@ -19,6 +19,12 @@ const Show = () => {
 
     fetchData();
   }, [showId]);
+  return { showData, showError };
+};
+
+const Show = () => {
+  const { showId } = useParams();
+  const { showData, showError } = useShowById(showId);
 
   if (showError) {
     return <div>We have an error: {showError.message}</div>;
