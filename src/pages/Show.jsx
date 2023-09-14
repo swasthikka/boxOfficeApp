63fd445384b0243478fce67e2b9cd495d151485c
swasthikka <swasthikka@gmail.com>
2023-09-14 07:23:41 +0530

    removed custom hook and used react query

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,30 +1,15 @@
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
-
-//custom hook
-const useShowById = showId => {
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        setShowData(data);
-      } catch (err) {
-        setShowData(null);
-      }
-    }
-
-    fetchData();
-  }, [showId]);
-  return { showData, showError };
-};
+import { useQuery } from '@tanstack/react-query';
 
 const Show = () => {
   const { showId } = useParams();
-  const { showData, showError } = useShowById(showId);
+
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ['show', 'showId'],
+    queryFn: () => getShowById(showId),
+  });
 
   if (showError) {
     return <div>We have an error: {showError.message}</div>;
